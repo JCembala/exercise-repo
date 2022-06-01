a364a0a2b0a54de990fba4396d65115ae8a4f2fd
JCembala <cembala.jakub@gmail.com>
2022-06-01 11:34:55 +0200

    chore #1: Update edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :current_password)
+    end
+  end
 end
