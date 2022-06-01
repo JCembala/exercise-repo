bfccbf09158c7068d29364d070d7c237866d1b14
JCembala <cembala.jakub@gmail.com>
2022-06-01 07:18:35 +0200

    feat #1: Add signIn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'users#index'
+
   get '/users', to: 'users#index'
+  
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
