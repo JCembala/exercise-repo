b669a11b518e5ee60eedcd2d970a8a66aa75cbf9
JCembala <cembala.jakub@gmail.com>
2022-06-02 09:48:49 +0200

    fix #1: Config for mailing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Mailcatcher config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
