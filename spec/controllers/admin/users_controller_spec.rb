cc668fff6f3e5f44c8cc284e1c07d590c48e76e0
JCembala <cembala.jakub@gmail.com>
2022-07-12 17:37:54 +0200

    chore #6: Changes based on PR comment
    added some more 'it' so almost every one has just one expect

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Admin::UsersController do
   describe 'GET /index' do
     context 'when user is not logged in' do
-      it 'redirects to root and returns 302' do
+      it 'redirects to /users/sign_in and returns 302' do
         user = create(:user)
         get :edit, params: { id: user.id }
 
+        expect(response).to redirect_to '/users/sign_in'
         expect(response.status).to eq(302)
       end
     end
@@ -19,12 +20,11 @@ RSpec.describe Admin::UsersController do
 
           get :index
 
-          expect(request.path).to eq admin_users_path
           expect(response).to be_successful
         end
 
         context 'when user is not an admin' do
-          it 'redirects to root and returns 302' do
+          it 'redirects to root_path' do
             user = create(:user)
             sign_in user
 
@@ -34,30 +34,15 @@ RSpec.describe Admin::UsersController do
             expect(response.status).to eq(302)
           end
 
-          it 'alert is present' do
+          it 'displays an alert' do
             user = create(:user)
             sign_in user
 
             get :index
 
-            expect(flash[:alert]).to be_present
+            expect(flash[:alert]).to eq(I18n.t('not_authorized'))
           end
         end
-      end
-    end
-  end
-
-  describe 'GET /update' do
-    context 'when user is an admin' do
-      it 'returns 302' do
-        admin = create(:user, :admin)
-        user = create(:user)
-        sign_in admin
-
-        get :update, params: { id: user.id }
-
-        expect(request.path).to eq "/admin/users/#{user.id}"
-        expect(response.status).to eq(302)
       end
     end
   end
@@ -88,14 +73,14 @@ RSpec.describe Admin::UsersController do
         expect(response.status).to eq(302)
       end
 
-      it 'alert is present' do
+      it 'displays an alert with not_authorized message' do
         user = create(:user)
         other_user = create(:user)
         sign_in user
 
         get :edit, params: { id: other_user.id }
 
-        expect(flash[:alert]).to be_present
+        expect(flash[:alert]).to eq(I18n.t('not_authorized'))
       end
     end
   end
@@ -104,34 +89,58 @@ RSpec.describe Admin::UsersController do
     context 'when user is an admin' do
       it 'updates successfully other user' do
         admin = create(:user, :admin)
-        user = create(:user)
+        user = create(:user, first_name: 'Joe', last_name: 'Ben')
         sign_in admin
 
         patch :update, params: { id: user.id, user: { first_name: 'Mike', last_name: 'Bang' } }
         user.reload
 
-        expect(response).to redirect_to '/admin/users'
         expect(user).to have_attributes(
           first_name: 'Mike',
           last_name: 'Bang'
         )
       end
+
+      it 'display an notice with message user.updated' do
+        admin = create(:user, :admin)
+        user = create(:user, first_name: 'Joe', last_name: 'Ben')
+        sign_in admin
+
+        patch :update, params: { id: user.id, user: { first_name: 'Mike', last_name: 'Bang' } }
+        user.reload
+
+        expect(flash[:notice]).to eq(I18n.t('user.updated'))
+      end
     end
 
     context 'when user is not an admin' do
-      it 'redirects to root' do
+      it 'returns 302' do
         user = create(:user)
         user_to_patch = create(:user, first_name: 'Joe', last_name: 'Ben')
         sign_in user
 
-        patch :update, params: { id: user.id, user: { first_name: 'Mike', last_name: 'Bang' } }
+        patch :update, params: {
+          id: user_to_patch.id,
+          user: { first_name: 'Mike', last_name: 'Bang' }
+        }
         user_to_patch.reload
 
         expect(response).to redirect_to :root
-        expect(user_to_patch).to have_attributes(
-          first_name: 'Joe',
-          last_name: 'Ben'
-        )
+        expect(response.status).to eq(302)
+      end
+
+      it 'displays an alert with not_authorized message' do
+        user = create(:user)
+        user_to_patch = create(:user, first_name: 'Joe', last_name: 'Ben')
+        sign_in user
+
+        patch :update, params: {
+          id: user_to_patch.id,
+          user: { first_name: 'Mike', last_name: 'Bang' }
+        }
+        user_to_patch.reload
+
+        expect(flash[:alert]).to eq(I18n.t('not_authorized'))
       end
     end
   end
